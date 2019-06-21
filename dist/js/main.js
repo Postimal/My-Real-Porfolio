3156b90eaa0af98546a0e522c944f764d2b6ea7e
Postimal <39851098+Postimal@users.noreply.github.com>
2019-06-21 17:49:23 +0200

    Feature: Project, skills

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,3 +17,10 @@ function toggleMenu() {
 
 
 menuBtn.addEventListener('click', toggleMenu);
+
+// button learn more 
+
+const hiddenInfo = document.querySelector('.learn-more');
+document.querySelector('.learn-more-btn').addEventListener("click", function(){
+    hiddenInfo.classList.toggle('show')
+})
