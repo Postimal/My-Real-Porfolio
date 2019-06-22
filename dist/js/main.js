0824aa1fb56a49d18963f64fe22adec502896046
Michal <michal.postek1@wp.pl>
2019-06-22 18:16:56 +0200

    Feature: Contact added, english text added

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,6 +21,7 @@ menuBtn.addEventListener('click', toggleMenu);
 // button learn more 
 
 const hiddenInfo = document.querySelector('.learn-more');
-document.querySelector('.learn-more-btn').addEventListener("click", function(){
+document.querySelector('.learn-more-btn').addEventListener("click", function () {
     hiddenInfo.classList.toggle('show')
 })
+
