a0e9b3bcfd3fe776698721927d4b814f50148a39
Michal <michal.postek1@wp.pl>
2019-07-16 20:44:56 +0200

    Fix: additional info, css bug delete

--- dist/js/socialMediaDetails.js
+++ dist/js/socialMediaDetails.js
@@ -1,32 +1,36 @@
 const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
 const hookPoint = document.querySelector('.top');
+const socialMediaResolution = window.matchMedia("(min-width: 650px)");
 
-function handleEnter() {
-    this.classList.add('trigger-enter');
-    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
-    background.classList.add('open');
+if (socialMediaResolution.matches) {
+    function handleEnter() {
+        this.classList.add('trigger-enter');
+        setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+        background.classList.add('open');
 
-    const dropdown = this.querySelector('.dropdown');
-    const dropdownCoords = dropdown.getBoundingClientRect();
-    const navCoords = hookPoint.getBoundingClientRect();
+        const dropdown = this.querySelector('.dropdown');
+        const dropdownCoords = dropdown.getBoundingClientRect();
+        const navCoords = hookPoint.getBoundingClientRect();
 
-    const coords = {
-        height: dropdownCoords.height,
-        width: dropdownCoords.width,
-        top: dropdownCoords.top - navCoords.top,
-        left: dropdownCoords.left - navCoords.left
-    };
-    console.log(coords)
+        const coords = {
+            height: dropdownCoords.height,
+            width: dropdownCoords.width,
+            top: dropdownCoords.top - navCoords.top,
+            left: dropdownCoords.left - navCoords.left
+        };
 
-    background.style.setProperty('width', `${coords.width}px`);
-    background.style.setProperty('height', `${coords.height}px`);
-    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
-}
+        background.style.setProperty('width', `${coords.width}px`);
+        background.style.setProperty('height', `${coords.height}px`);
+        background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+    }
 
-function handleLeave() {
-    this.classList.remove('trigger-enter', 'trigger-enter-active');
-    background.classList.remove('open');
+
+
+    function handleLeave() {
+        this.classList.remove('trigger-enter', 'trigger-enter-active');
+        background.classList.remove('open');
+    }
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
